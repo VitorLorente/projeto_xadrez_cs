5551ae8337d6aa766762ff06debf9fcd0717cf66
VitorLorente <lorente.sociologia@gmail.com>
2017-12-29 00:47:03 -0200

    Impressão do tabuleiro da tela (Classe Tela)

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -4,9 +4,11 @@ using tabuleiro;
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-
             Tabuleiro tab = new Tabuleiro(8, 8);
+            Tela.imprimirTabuleiro(tab);
             Console.ReadLine();
         }
     }
+
+
 }
