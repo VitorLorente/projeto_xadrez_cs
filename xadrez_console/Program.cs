6ee004f9ca31103d32acc4d880301facb747d211
VitorLorente <lorente.sociologia@gmail.com>
2017-12-29 00:26:49 -0200

    Classe Peca, com matriz de tabuleiro, cor e pocição

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -1,14 +1,11 @@
 ﻿using System;
-using Tabuleiro;
+using tabuleiro;
 
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-            Posicao P;
 
-            P = new Posicao(2, 3);
-
-            Console.WriteLine(P);
+            Tabuleiro tab = new Tabuleiro(8, 8);
             Console.ReadLine();
         }
     }
