d7bdeee8f5bcbfad0ccd6410d0e3dd45fd36f8cb
VitorLorente <lorente.sociologia@gmail.com>
2017-12-28 23:22:45 -0200

    Versão com classe Posicao criada

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -1,6 +1,15 @@
-﻿namespace xadrez_console {
+﻿using System;
+using Tabuleiro;
+
+namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
+            Posicao P;
+
+            P = new Posicao(2, 3);
+
+            Console.WriteLine(P);
+            Console.ReadLine();
         }
     }
 }
