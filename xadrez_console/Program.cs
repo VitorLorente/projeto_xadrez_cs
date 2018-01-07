9e38518d78b449476db82530c73aadb62944284b
VitorLorente <lorente.sociologia@gmail.com>
2018-01-07 20:06:38 -0200

    Conversão de posição de matriz para xadrez

--- xadrez_console/Program.cs
+++ xadrez_console/Program.cs
@@ -5,7 +5,7 @@ using xadrez;
 namespace xadrez_console {
     class Program {
         static void Main(string[] args) {
-            Tabuleiro tab = new Tabuleiro(8, 8);
+            /*Tabuleiro tab = new Tabuleiro(8, 8);
             try {
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
@@ -14,7 +14,11 @@ namespace xadrez_console {
             }
             catch(TabuleiroException e) {
                 Console.WriteLine(e.Message);
-            }
+            }*/
+
+            PosicaoXadrez posX = new PosicaoXadrez('a', 1);
+            Console.WriteLine(posX);
+            Console.WriteLine(posX.toPosicao());
 
             Console.ReadLine();
         }
